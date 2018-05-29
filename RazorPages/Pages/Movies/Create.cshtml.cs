0a0f33476453bc7871d697a628e5d7e5e6856f73
Steve Baker <ste.baker.92@gmail.com>
2018-05-29 14:05:02 +0100

    Inital commit & db setup

---
+++ RazorPages/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPages.Models;
+
+namespace RazorPages.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPages.Models.MovieContext _context;
+
+        public CreateModel(RazorPages.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPages/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Models;
+
+namespace RazorPages.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPages.Models.MovieContext _context;
+
+        public IndexModel(RazorPages.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
